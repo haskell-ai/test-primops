ed39a2223ce84b3d9bf3aba97358176e51cc280b
Ben Gamari <ben@smart-cactus.org>
2021-11-07 18:45:10 -0500

    run: Pass --allow-newer to run-it build

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@ set -e -o pipefail
 BOOT_GHC="${BOOT_GHC:-ghc}"
 TEST_GHC="${TEST_GHC:-ghc}"
 
-cabal build -w "$TEST_GHC" run-it
-RUNIT="$(cabal list-bin -w "$TEST_GHC" run-it)"
+ALLOW_NEWER="--allow-newer=base"
+cabal build $ALLOW_NEWER -w "$TEST_GHC" run-it
+RUNIT="$(cabal list-bin $ALLOW_NEWER -w "$TEST_GHC" run-it)"
+echo "runit is $RUNIT"
 
 cabal run -w "$BOOT_GHC" test-primops -- \
     --ghc-path="$TEST_GHC" \
