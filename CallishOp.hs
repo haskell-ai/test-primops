11607cae96de2309ed53c636f5ad0267312df7a2
Ben Gamari <ben@smart-cactus.org>
2021-11-07 09:37:26 -0500

    Callish: generalize

--- CallishOp.hs
+++ CallishOp.hs
@@ -3,47 +3,93 @@ module CallishOp where
 import Numeric.Natural
 import Data.Bits
 import Test.QuickCheck
+import Data.Foldable (foldl')
 
 import Width
 import ToCmm
 import Number
 import Expr
 
-popcnt :: forall w. (KnownWidth w) => UnaryCallish w WordSize
-popcnt = UnaryCallish
+popcnt :: forall w. (KnownWidth w) => Callish (Expr w) WordSize
+popcnt = Callish
     { name = "%popcnt" ++ show (widthBits (knownWidth @w))
-    , refImpl = fromUnsigned . fromIntegral . popCount . toUnsigned
+    , refImpl = fromUnsigned . fromIntegral . popCount . toUnsigned . interpret
     }
 
+-- | Arguments are @(mask, source)@.
+pdep :: forall w. (KnownWidth w) => Callish (Expr w, Expr w) WordSize
+pdep = Callish
+    { name = "%pdep" ++ show (widthBits (knownWidth @w))
+    , refImpl = uncurry ref
+    }
+  where
+    ref :: Expr w -> Expr w -> Number WordSize
+    ref mask0 x0 = fromUnsigned $ fromBits $ go (exprBits mask0) (exprBits x0)
+      where
+        exprBits = toBits . interpret
 
-prop_unary_callish_correct
-    :: forall w. (KnownWidth w)
-    => UnaryCallish w WordSize
-    -> Expr w
+        go (True:mask)  (b:rest) = b     : go mask rest
+        go (False:mask) rest     = False : go mask rest
+        go []           _        = []
+        go _            []       = error "pdep: ran out of bits"
+
+-- | Arguments are @(mask, source)@.
+pext :: forall w. (KnownWidth w) => Callish (Expr w, Expr w) WordSize
+pext = Callish
+    { name = "%pext" ++ show (widthBits (knownWidth @w))
+    , refImpl = uncurry ref
+    }
+  where
+    ref :: Expr w -> Expr w -> Number WordSize
+    ref mask x =
+        fromUnsigned
+        $ fromBits
+        [ b | (True, b) <- zip (exprBits mask) (exprBits x) ]
+      where
+        exprBits = toBits . interpret
+
+toBits :: forall w. (KnownWidth w) => Number w -> [Bool]
+toBits x =
+    [ x `testBit` i | i <- [0..widthBits (knownWidth @w)-1] ]
+
+fromBits :: [Bool] -> Natural
+fromBits bits = foldl' (.|.) 0 [ bit i | (i, True) <- zip [0..] bits ]
+
+prop_callish_correct
+    :: forall args. (CmmArgs args)
+    => Callish args WordSize
+    -> args
     -> Property
-prop_unary_callish_correct op e = ioProperty $ do
-    r <- evalUnaryCallish op e
-    return $ refImpl op (interpret e) === fromUnsigned r
+prop_callish_correct op e = ioProperty $ do
+    r <- evalCallish op e
+    return $ refImpl op e === r
 
-data UnaryCallish w r
-    = UnaryCallish { name :: String
-                   , refImpl :: Number w -> Number r
-                   }
+data Callish args result
+    = Callish { name :: String
+              , refImpl :: args -> Number result
+              }
 
-evalUnaryCallish
-    :: forall w. (KnownWidth w)
-    => UnaryCallish w WordSize
-    -> Expr w
-    -> IO Natural
-evalUnaryCallish op e =
-    evalCmm ["-dcmm-lint"] cmm
+class CmmArgs arg where
+    argsToCmm :: arg -> String
+
+instance (CmmArgs a, CmmArgs b) => CmmArgs (a,b) where
+    argsToCmm (a,b) = concat ["(", argsToCmm a, ", ", argsToCmm b, ")"]
+
+instance (KnownWidth w) => CmmArgs (Expr w) where
+    argsToCmm a = exprToCmm a
+
+evalCallish
+    :: forall args. (CmmArgs args)
+    => Callish args WordSize
+    -> args
+    -> IO (Number WordSize)
+evalCallish op args =
+    fromUnsigned <$> evalCmm ["-dcmm-lint"] cmm
   where
     cmm = unlines
         [ "test ( bits64 buffer ) {"
         , "  bits64 ret;"
-        , "  " ++ cmmType (knownWidth @w) ++ " x;"
-        , "  x = " ++ exprToCmm e ++ ";"
-        , "  (ret) = prim " ++ name op ++ "(x);"
+        , "  (ret) = prim " ++ name op ++ argsToCmm args ++ ";"
         , "  return (ret);"
         , "}"
         ]
