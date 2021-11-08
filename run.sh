5504fdf30b11392978c9a23308e2b7b9a3a42e8a
Ben Gamari <ben@smart-cactus.org>
2021-11-08 14:12:18 -0500

    run: Add repl mode

--- run.sh
+++ run.sh
@@ -5,12 +5,37 @@ set -e -o pipefail
 BOOT_GHC="${BOOT_GHC:-ghc}"
 TEST_GHC="${TEST_GHC:-ghc}"
 
-ALLOW_NEWER="--allow-newer=base"
-cabal build $ALLOW_NEWER -w "$TEST_GHC" run-it
-RUNIT="$(cabal list-bin $ALLOW_NEWER -w "$TEST_GHC" run-it)"
-echo "runit is $RUNIT"
+build_runit() {
+    ALLOW_NEWER="--allow-newer=base"
+    cabal build $ALLOW_NEWER -w "$TEST_GHC" run-it
+    RUNIT="$(cabal list-bin $ALLOW_NEWER -w "$TEST_GHC" run-it)"
+    echo "runit is $RUNIT"
+}
 
-cabal run -w "$BOOT_GHC" test-primops -- \
-    --ghc-path="$TEST_GHC" \
-    --run-it-path="$RUNIT" \
-    $@
+run() {
+    build_runit
+    cabal run -w "$BOOT_GHC" test-primops -- \
+        --ghc-path="$TEST_GHC" \
+        --run-it-path="$RUNIT" \
+        $@
+}
+
+repl() {
+    build_runit
+    cat >.ghci <<EOF
+let ghcPath = "$TEST_GHC";
+let runItPath = "$RUNIT";
+let comp = (basicCompiler ghcPath) { compRunIt = runItPath };
+putStrLn "Hello world"
+putStrLn $ "Compiler under test is " ++ ghcPath
+
+EOF
+    cabal repl -w "$BOOT_GHC" test-primops
+}
+
+mode="$1"
+case $mode in
+  "") run ;;
+  run) shift; run ;;
+  repl) shift; repl ;;
+esac
