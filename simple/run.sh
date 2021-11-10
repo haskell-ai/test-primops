9dae29640a7cb823452913594c5da88dd9cef9f5
Ben Gamari <ben@smart-cactus.org>
2021-11-10 10:34:50 -0500

    simple/run: Clean dist-newstyle

--- simple/run.sh
+++ simple/run.sh
@@ -5,6 +5,7 @@ set -e -x
 TEST_GHC="${TEST_GHC:-ghc}"
 CABAL="${CABAL:-cabal}"
 
+rm -Rf dist-newstyle
 "$CABAL" build -w "$TEST_GHC" --ghc-options="$@" -v3
 exe="$("$CABAL" list-bin -w "$TEST_GHC" --ghc-options="$@" simple)"
 gdb "$exe" \
