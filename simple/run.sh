4b2b0dd1eb9f7dafcff9c65231d638d8af9440f7
Sven Tennie <sven.tennie@gmail.com>
2023-06-30 19:33:25 +0200

    Use an emulator (QEMU) to run "simple"
    This is useful for cross-platform development. Currently, the only
    targeted emulator is QEMU.

--- simple/run.sh
+++ simple/run.sh
@@ -4,19 +4,32 @@ set -e -x
 
 TEST_GHC="${TEST_GHC:-ghc}"
 CABAL="${CABAL:-cabal}"
+# QEMU executable
+EMULATOR="${EMULATOR:-}"
 
 rm -Rf dist-newstyle
 args=(
     "-w$TEST_GHC"
     "--ghc-options=$@"
     "--disable-optimization"
-    "-v3"
 )
 
-"$CABAL" build "${args[@]}" simple
-exe="$("$CABAL" list-bin "${args[@]}" simple)"
+"$CABAL" build "${args[@]}" -v3 simple
+exe=$("$CABAL" list-bin "${args[@]}" simple)
 
-gdb "$exe" \
-    -ex "break test" \
-    -ex "run" \
-    -ex "disassemble"
+echo "Using executable: $exe"
+
+if [ -n $EMULATOR ]; then
+    $EMULATOR -g 1111 $exe &
+    gdb \
+        -ex "file $exe" \
+        -ex "break test" \
+        -ex "target remote localhost:1111" \
+        -ex "continue" \
+        -ex "disassemble"
+else
+    gdb "$exe" \
+        -ex "break test" \
+        -ex "run" \
+        -ex "disassemble"
+fi
